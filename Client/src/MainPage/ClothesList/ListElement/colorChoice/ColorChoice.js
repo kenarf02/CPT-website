1e962e8c56f451771292fb8a87f4a0a995b8e3be
kenarf02 <56699656+kenarf02@users.noreply.github.com>
2020-07-30 11:49:18 +0200

    changed colorchoice to not bug

--- Client/src/MainPage/ClothesList/ListElement/colorChoice/ColorChoice.js
+++ Client/src/MainPage/ClothesList/ListElement/colorChoice/ColorChoice.js
@@ -1,27 +1,37 @@
 import React from 'react';
 import './ColorChoice.css';
+
 class ColorPicker extends React.Component{
 state={
-    colors: ['red','blue','green']
+    colors: ['red','blue','green'],
+    focusedID: 0
 }
 
-setColor (colors,buttonToFocus){
+unfocus(){
+    document.getElementById("focused").setAttribute("id",this.state.focusedID);
+}
+ 
+setColor (colors,buttonToFocusId){
     this.props.colorSet(colors)
-
+    this.unfocus(buttonToFocusId)
+    this.setState({focusedID:buttonToFocusId});
+    document.getElementById(buttonToFocusId.toString()).setAttribute("id","focused")
     console.log(colors)
 }
+
 componentDidMount(){
     this.setState({
-        colors : ['black','white','red','blue','green','coral','wheat']
+        //TODO: change this array into object where colors have polish names
+        colors : ['Black','White','Red','Blue','Green','Coral','Wheat']
     });
-    document.getElementById('0').focus();
+    document.getElementById(this.state.focusedID.toString()).setAttribute("id","focused");
 
 }
     render(){
         return(
-            <div className="Color-choice">
+            <div className="Color-choice"> 
              {  this.state.colors.map((currElemenent,index)=>(<button className= "Color" id={index} 
-             onClick = {() => this.setColor(currElemenent)}
+             onClick = {() => this.setColor(currElemenent,index)}
                 style = {{backgroundColor: currElemenent}}
                 />
                ))}
